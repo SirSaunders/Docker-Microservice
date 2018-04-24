e6250ff616ffa45dc881dc9b3c4bfcd6725b3a7e
CornyElephant <eldraco@mac.com>
2018-04-24 19:54:22 -0400

    FINISHED COMPARATOR SERVICE

---
+++ ComparatorService/src/main/java/comparator/ComparatorController.java
@@ -0,0 +1,14 @@
+package comparator;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ComparatorController {
+
+    @RequestMapping("/compare")
+    public int compare(@RequestParam(value="val1", defaultValue="World") String val1, @RequestParam(value="val2", defaultValue="World") String val2) {
+        return val1.compareTo(val2);
+    }
+}

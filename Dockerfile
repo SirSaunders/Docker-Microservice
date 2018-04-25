0fb7161fe4c0e8b944bca2dc3caf5669c66e79c5
Johnathan Saunders <Saundersokj@gmail.com>
2018-04-25 00:05:54 -0400

    fixed docker stuff for using Spring in docker-compose

--- Dockerfile
+++
@@ -1,7 +0,0 @@
-FROM java:8
-
-## build spring project
-COPY ./SpringApp/  /usr/local/spring/
-RUN cd /usr/local/spring/ && ls
-RUN cd /usr/local/spring/ && ./gradlew build && java -jar build/libs/gs-rest-service-0.1.0.jar & disown
-RUN echo("done")
--- SpringApp/src/main/java/hello/GreetingController.java
+++ SpringApp/src/main/java/hello/GreetingController.java
@@ -1,9 +1,9 @@
 package hello;
 
-import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicLong;
 
 @RestController
 public class GreetingController {
@@ -11,9 +11,8 @@ public class GreetingController {
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
-    @RequestMapping("/greeting")
-    public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
-        return new Greeting(counter.incrementAndGet(),
-                            String.format(template, name));
+    @RequestMapping("/")
+    public String greeting() {
+        return  "helloworld";
     }
 }
